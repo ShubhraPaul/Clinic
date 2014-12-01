5e40e89569b1102ea892e7132404666d204f513d
Falaleev Maxim <max@studio107.ru>
2014-12-01 16:06:54 +0300

    First

---
+++ Forms/DepartmentForm.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 28/11/14 18:05
+ */
+
+namespace Modules\Clinic\Forms;
+
+use Mindy\Form\ModelForm;
+use Modules\Clinic\Models\Department;
+
+class DepartmentForm extends ModelForm
+{
+    public function getModel()
+    {
+        return new Department;
+    }
+
+    public function getInlines()
+    {
+        return [
+            ['department' => ImageForm::className()]
+        ];
+    }
+}
